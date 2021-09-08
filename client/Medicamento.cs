ee7dfcc687203e1ed1027e3de62fee9d0b3b5a2d
damianorsi1 <dorsi@andreani.com>
2021-09-08 09:31:15 -0300

    agrego todo lo que hice hasta el momento

---
+++ client/Medicamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace client
+{
+    class Medicamento
+    {
+        static void Main(string[] args)
+        {
+            // Este conmutador debe establecerse antes de crear GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("http://localhost:5500");
+            //Canal que se conecta a la direccion en la cual se va a encontrar mi servicio
+            var client = new Farmacia.FarmaciaClient(channel);
+
+            var medicamento = new Medicamento{id = 1, nombre = "Abaloparatida", 2, codigo = "DFR-64938-3", tipo = "colirio"};
+        }
+    }
+}
